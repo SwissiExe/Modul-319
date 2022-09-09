d4abbc5d48a351e9e3c3bda3492bd6b24e92b874
SwissiExe <55888734+SwissiExe@users.noreply.github.com>
2022-09-09 07:53:02 +0200

    Add files via upload

---
+++ Hello_Wolrd Kopie/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+
+
+
+
+        System.out.println("Meine Personalien");
+        System.out.println(" ---------------------");
+        System.out.println("Name: Joel Furter");
+        System.out.println("Alter: 16");
+        System.out.print("ich bin am: 17.11.2005");
+        System.out.print(" 17.11.2005 ");
+        System.out.println("Geboren");
+        System.out.print("Ich Wohne in: ");
+        System.out.print(8259);
+        System.out.println(" Kaltenbach");
+        System.out.println("E-Mail: Joel.Furter@gmail.com");
+        System.out.println(" ");
+
+
+    }
+
+
+}
